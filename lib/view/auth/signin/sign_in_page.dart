40ba789b3ee4b25ce8122fce5b0768981a934f6c
victor <victorhugobbpereira02@gmail.com>
2025-02-24 14:45:32 -0300

    scroll do formulario quando aberto

--- lib/view/auth/signin/sign_in_page.dart
+++ lib/view/auth/signin/sign_in_page.dart
@@ -24,7 +24,6 @@ class _SignInPageState extends State<SignInPage> with ValidationMixinClass {
   Widget build(BuildContext context) {
     final authViewModel = Provider.of<AuthViewModel>(context);
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: AppTheme.lightTheme.primaryColor,
       body: SafeArea(
         child: Container(
@@ -42,86 +41,88 @@ class _SignInPageState extends State<SignInPage> with ValidationMixinClass {
             spacing: 40.0,
             children: [
               Expanded(child: Center(child: Image.asset('assets/images/write_techcontrol.png'))),
-              Form(
-                key: _signInKeyForm,
-                child: Column(
-                  spacing: 10.0,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 3.0,
-                      children: [
-                        Text(
-                          'Email',
-                          style: TextStyle(
-                            fontSize: TextTheme.of(context).titleMedium!.fontSize,
-                            color: AppTheme.lightTheme.colorScheme.surface,
-                          ),
-                        ),
-                        TextFormFieldWidget(
-                          autofocus: true,
-                          controller: _emailController,
-                          inputBorderType: OutlineInputBorder(),
-                          inputType: TextInputType.emailAddress,
-                          obscure: false,
-                          sizeInputBorder: 2.0,
-                          fillColor: AppTheme.lightTheme.colorScheme.primary,
-                          filled: true,
-                          validator:
-                              (value) => combine([
-                                () => isNotEmpyt(value),
-                                () =>
-                                    EmailValidator.validate(value.toString())
-                                        ? null
-                                        : "Email inválido!",
-                                () => maxTwoHundredCharacters(value),
-                              ]),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 3.0,
-                      children: [
-                        Text(
-                          'Senha',
-                          style: TextStyle(
-                            fontSize: TextTheme.of(context).titleMedium!.fontSize,
-                            color: AppTheme.lightTheme.colorScheme.surface,
-                          ),
-                        ),
-                        TextFormFieldWidget(
-                          autofocus: true,
-                          controller: _passwordController,
-                          inputBorderType: OutlineInputBorder(),
-                          inputType: TextInputType.emailAddress,
-                          obscure: _obscurePassword,
-                          sizeInputBorder: 2.0,
-                          fillColor: AppTheme.lightTheme.colorScheme.primary,
-                          filled: true,
-                          iconSuffix: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _obscurePassword = !_obscurePassword; // Alterna o estado
-                              });
-                            },
-                            icon: Icon(
-                              _obscurePassword
-                                  ? Icons.visibility_off_outlined
-                                  : Icons.visibility_outlined,
+              SingleChildScrollView(
+                reverse: true,
+                child: Form(
+                  key: _signInKeyForm,
+                  child: Column(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 3.0,
+                        children: [
+                          Text(
+                            'Email',
+                            style: TextStyle(
+                              fontSize: TextTheme.of(context).titleMedium!.fontSize,
                               color: AppTheme.lightTheme.colorScheme.surface,
                             ),
                           ),
-                          validator:
-                              (value) => combine([
-                                () => isNotEmpyt(value),
-                                () => hasSixChars(value),
-                                () => maxTwoHundredCharacters(value),
-                              ]),
-                        ),
-                      ],
-                    ),
-                  ],
+                          TextFormFieldWidget(
+                            autofocus: true,
+                            controller: _emailController,
+                            inputBorderType: OutlineInputBorder(),
+                            inputType: TextInputType.emailAddress,
+                            obscure: false,
+                            sizeInputBorder: 2.0,
+                            fillColor: AppTheme.lightTheme.colorScheme.primary,
+                            filled: true,
+                            validator:
+                                (value) => combine([
+                                  () => isNotEmpyt(value),
+                                  () =>
+                                      EmailValidator.validate(value.toString())
+                                          ? null
+                                          : "Email inválido!",
+                                  () => maxTwoHundredCharacters(value),
+                                ]),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 3.0,
+                        children: [
+                          Text(
+                            'Senha',
+                            style: TextStyle(
+                              fontSize: TextTheme.of(context).titleMedium!.fontSize,
+                              color: AppTheme.lightTheme.colorScheme.surface,
+                            ),
+                          ),
+                          TextFormFieldWidget(
+                            autofocus: true,
+                            controller: _passwordController,
+                            inputBorderType: OutlineInputBorder(),
+                            inputType: TextInputType.emailAddress,
+                            obscure: _obscurePassword,
+                            sizeInputBorder: 2.0,
+                            fillColor: AppTheme.lightTheme.colorScheme.primary,
+                            filled: true,
+                            iconSuffix: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _obscurePassword = !_obscurePassword; // Alterna o estado
+                                });
+                              },
+                              icon: Icon(
+                                _obscurePassword
+                                    ? Icons.visibility_off_outlined
+                                    : Icons.visibility_outlined,
+                                color: AppTheme.lightTheme.colorScheme.surface,
+                              ),
+                            ),
+                            validator:
+                                (value) => combine([
+                                  () => isNotEmpyt(value),
+                                  () => hasSixChars(value),
+                                  () => maxTwoHundredCharacters(value),
+                                ]),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Expanded(
